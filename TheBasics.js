a8a600981a5656dc9b70e7ca2fc2519dba1b5016
Rizni Kabir <rizni1234@gmail.com>
2022-03-09 19:40:56 +0800

    Added questions to the page

--- TheBasics.js
+++ TheBasics.js
@@ -1,1 +1,23 @@
 //EXERCISES 
+
+//1. Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value
+
+//2. Use the arithmetic operators to determine the individual digits of a 4-digit number like 4936
+
+//3.  Identify the data type for each of the following values:
+
+//4. Explain why this console.log('5'+10) logs '510' instead of 15 
+
+//5. Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead
+
+//6. Use the template literal syntax along with the expression Number('5') + 10 to log the following sentence to the console
+
+//7. Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array?
+
+//8. Create an array named names that contains a list of pet names
+
+//9. Create an object named pets that contains a list of pet names and the type of animal
+
+//10.  What value does'foo' === 'Foo' evaluate to? 
+
+//11. 
