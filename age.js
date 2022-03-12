8c596a75e7a84dae130eb744bcff7d923b521a07
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 14:48:48 +0800

    Answered the third question in input/output chapt

--- age.js
+++ age.js
@@ -1,6 +1,30 @@
 // This is the second question in the variable section
 
-const age = 23;
+// const age = 23;
+
+// console.log(`You are ${age} years old.`)
+// console.log(`In 10 years, you will be ${10+age} years old.`);
+// console.log(`In 20 years, you will be ${20+age} years old.`);
+// console.log(`In 30 years, you will be ${30+age} years old.`);
+// console.log(`In 40 years, you will be ${40+age} years old.`);
+
+
+// This is the third question in the input/output section of the book
+
+let rlSync = require('readline-sync');
+
+// let age = rlSync.question("How old are you?\n");
+
+// console.log(`You are ${age} years old.`)
+// console.log(`In 10 years, you will be ${10+age} years old.`);
+// console.log(`In 20 years, you will be ${20+age} years old.`);
+// console.log(`In 30 years, you will be ${30+age} years old.`);
+// console.log(`In 40 years, you will be ${40+age} years old.`);
+// This way will return a string because the user input in the age variable is a string and not a number
+
+let age = rlSync.question("How old are you?\n");
+age = parseInt(age);
+// or you can do let age = Number(rlSync.question("How old are you?\n");)
 
 console.log(`You are ${age} years old.`)
 console.log(`In 10 years, you will be ${10+age} years old.`);
