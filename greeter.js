aa854322c52aaea5a05206932701ff2f38a586fe
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 14:49:32 +0800

    Answered 1 and 2 of the third section

--- greeter.js
+++ greeter.js
@@ -1,7 +1,28 @@
 // First question in the variable section of the book
 
-const firstName = 'Victor';
+// let rlSync = require('readline-sync');
 
-console.log(`Good Morning, ${firstName}.`);
-console.log(`Good Afternoon, ${firstName}.`);
-console.log(`Good Evening, ${firstName}.`);
+// const firstName = 'Victor';
+
+// console.log(`Good Morning, ${firstName}.`);
+// console.log(`Good Afternoon, ${firstName}.`);
+// console.log(`Good Evening, ${firstName}.`);
+
+// let userName = rlSync.question("What's your name?\n");
+// console.log(`Good morning, ${userName}`);
+
+//First question in the input/output section of the book 
+
+let rlSync = require('readline-sync');
+
+// let name = rlSync.question("What's your name?\n");
+// console.log(`Hello, ${name}`);
+
+//Second question in the input/output section of the book
+
+let fName = rlSync.question("What's your first name?\n");
+let lName = rlSync.question("What's your last name?\n");
+
+let fullName = fName + " " + lName;
+
+console.log(`Hello, ${fullName}`);
