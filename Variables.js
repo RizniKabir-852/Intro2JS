a12427e0551ec38417be414df784eff5da193aaa
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 13:15:18 +0800

    Wrote down the questions for the assignment

--- Variables.js
+++ Variables.js
@@ -1,1 +1,45 @@
 //Second section of the book
+
+/*
+
+1. Write a program named greeter.js that greets Vicotr three timess
+2. Write a program named age.js that includes someone's age and then calculates and reports the future age in 10,20,30, & 40 years
+3. What happens when you run :
+
+{
+  let foo = 'bar';
+}
+
+console.log(foo);
+
+Why do we get that result?
+
+4. What happens when you run:
+
+const NAME = 'Victor';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+NAME = 'Joe';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+5. What does this program log to console and why:
+
+let foo = 'bar';
+{
+  let foo = 'qux';
+}
+
+console.log(foo);
+
+6. Will this program produce an error when run? WHy or why not?
+const FOO = 'bar';
+{
+  const FOO = 'qux';
+}
+
+console.log(FOO);
+*/
