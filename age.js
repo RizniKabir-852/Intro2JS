c0476226db7e3a14ac816bd211a4439ef884df2b
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 13:33:16 +0800

    the second question in the variable exercise

---
+++ age.js
@@ -0,0 +1,9 @@
+// This is the second question in the variable section
+
+const age = 23;
+
+console.log(`You are ${age} years old.`)
+console.log(`In 10 years, you will be ${10+age} years old.`);
+console.log(`In 20 years, you will be ${20+age} years old.`);
+console.log(`In 30 years, you will be ${30+age} years old.`);
+console.log(`In 40 years, you will be ${40+age} years old.`);
