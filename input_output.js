9cf29481546dbe2c2639c435d9ecd61c4b9bfed6
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 14:51:01 +0800

    Wrote down the questions

---
+++ input_output.js
@@ -0,0 +1,11 @@
+//Third section of the book 
+
+/* 
+
+1. Write a dynamic greeter program named greeter.js. It should ask for a name and then output hello name
+
+2. Modify greeter.js to ask user for first name and then last name seperately and then greet them using their full name 
+
+3. Modify age.js to ask for their name 
+
+*/
