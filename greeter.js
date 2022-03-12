b7db554daf10f95b0f06f2fcacffad3dbfebe355
Rizni Kabir <rizni1234@gmail.com>
2022-03-12 13:33:33 +0800

    The first question in the variable exercise

---
+++ greeter.js
@@ -0,0 +1,7 @@
+// First question in the variable section of the book
+
+const firstName = 'Victor';
+
+console.log(`Good Morning, ${firstName}.`);
+console.log(`Good Afternoon, ${firstName}.`);
+console.log(`Good Evening, ${firstName}.`);
