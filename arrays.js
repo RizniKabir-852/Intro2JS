7c478b33cdb46db76bdf8304b9acf95fd9ce46f1
Rizni Kabir <rizni1234@gmail.com>
2022-03-21 14:29:55 +0800

    Answered Q1 for array exercises

---
+++ arrays.js
@@ -0,0 +1,33 @@
+// Chapter 7: Arrays
+
+//1. what are the final length values for array1, array2, array3, array4, and array5 ?
+
+/* 
+let array1 = [1, 2, undefined, 4];
+
+let array2 = [1];
+array2.length = 5;
+
+let array3 = [];
+array3[-1] = [1];
+
+let array4 = [1, 2, 3, 4, 5];
+array4.length = 3;
+
+let array5 = [];
+array5[100] = 3;
+*/
+
+/* Answer to Q1
+
+array1 has a length of 4 
+
+array2 has a length of 5
+
+array3 has a length of 1
+
+array4 has a length of 3
+
+array5 has a length of 3
+
+*/
