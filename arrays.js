6d8715142f1d16055e556beedf0b620ba42aae5f
Rizni Kabir <rizni1234@gmail.com>
2022-03-21 14:35:05 +0800

    Gave more details to my answers to Q1

--- arrays.js
+++ arrays.js
@@ -21,13 +21,18 @@ array5[100] = 3;
 /* Answer to Q1
 
 array1 has a length of 4 
+- undefined takes up a the space of array1[2]. That space is just not initialsied 
 
 array2 has a length of 5
+- it originally had a length of 1 but by assigning 5 to the length of the array, 4 more spaces were added into the list. 
 
-array3 has a length of 1
+array3 has a length of 0
+- you cannot access an index with a negative number   
 
 array4 has a length of 3
+- it removes everything in the list after the lenghtof 3
 
-array5 has a length of 3
+array5 has a length of 101
+- since the 100th index was picked inside an empty list, 100 spaces were created and the length of the list is the last index + 1
 
 */
